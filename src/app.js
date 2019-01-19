a4b2b6b09f9359aaefcc873b7710a26f6e0ec494
alfabet <setiawanalfabet@gmail.com>
2019-01-19 23:11:43 +0700

    add react-dom-router, refactoring folder

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieInfo from './pages/MovieInfo';
+import TopUp from './pages/TopUp';
 
-const App = () => {
-  return <div>Hello React!</div>;
-};
+const App = () => (
+  <Switch>
+    <Route exact path='/' component={MovieList}/>
+    <Route path='/movie-detail/:movieID' component={MovieInfo}/>
+    <Route path='/topup' component={TopUp}/>
+    <Route component={MovieList}/>
+  </Switch>
+);
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("app"));
